719cafd79021d8fdf4baed79b92dd12c90979a24
Peter Karman <perl@peknet.com>
2007-10-25 14:55:46 +0000

    Import of KARMAN/Rose-DBx-Garden-0.01 from CPAN.
    gitpan-cpan-distribution: Rose-DBx-Garden
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         KARMAN/Rose-DBx-Garden-0.01.tar.gz
    gitpan-cpan-author:       KARMAN
    gitpan-cpan-maturity:     released

---
+++ lib/Rose/DBx/Garden.pm
@@ -0,0 +1,516 @@
+package Rose::DBx::Garden;
+
+use warnings;
+use strict;
+use base qw( Rose::DB::Object::Loader );
+use Carp;
+use Data::Dump qw( dump );
+use Path::Class;
+use File::Slurp;
+use File::Basename;
+
+use Rose::Object::MakeMethods::Generic (
+    boolean => [
+        'find_schemas'  => { default => 1 },
+        'force_install' => { default => 0 },
+    ],
+    'scalar --get_set_init' => 'column_field_map',
+    'scalar --get_set_init' => 'column_to_label',
+    'scalar --get_set_init' => 'garden_prefix',
+    'scalar --get_set_init' => 'perltidy_opts',
+    'scalar --get_set_init' => 'base_code',
+);
+
+=head1 NAME
+
+Rose::DBx::Garden - bootstrap Rose::DB::Object and Rose::HTML::Form classes
+
+=head1 VERSION
+
+Version 0.01
+
+B<** DEVELOPMENT RELEASE -- API SUBJECT TO CHANGE **>
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+ use Rose::DBx::Garden;
+    
+ my $garden = Rose::DBx::Garden->new(
+ 
+         garden_prefix   => 'MyRoseGarden',    # instead of class_prefix
+         perltidy_opts   => '-pbp -nst -nse',  # Perl Best Practices
+         db              => My::DB->new, # Rose::DB object
+         find_schemas    => 0,           # set true if your db has schemas
+         force_install   => 0,           # do not overwrite existing files
+         # other Rose::DB::Object::Loader params here
+ 
+ );
+                        
+ # $garden ISA Rose::DB::Object::Loader
+                     
+ $garden->plant('path/to/where/i/want/files');
+
+=head1 DESCRIPTION
+
+Rose::DBx::Garden bootstraps Rose::DB::Object and Rose::HTML::Form based projects.
+The idea is that you can point the module at a database and end up with work-able
+RDBO and Form classes with a single method call.
+
+Rose::DBx::Garden inherits from Rose::DB::Object::Loader, so all the magic there
+is also available here.
+
+=head1 METHODS
+
+B<NOTE:> All the init_* methods are intended for when you subclass the Garden
+class. You can pass in values to the new() constructor for normal use.
+See L<Rose::Object::MakeMethods::Generic>.
+
+=cut
+
+=head2 init_column_field_map
+
+Sets the default RDBO column type to RHTMLO field type mapping.
+Should be a hash ref of 'rdbo' => 'rhtmlo' format.
+
+=cut
+
+sub init_column_field_map {
+    return {
+        'varchar'          => 'text',
+        'text'             => 'textarea',
+        'character'        => 'text',
+        'date'             => 'date',
+        'datetime'         => 'datetime',
+        'epoch'            => 'datetime',
+        'integer'          => 'integer',
+        'serial'           => 'hidden',
+        'time'             => 'time',
+        'timestamp'        => 'time',
+        'float'            => 'numeric',    # TODO nice to have ::Field::Float
+        'numeric'          => 'numeric',
+        'decimal'          => 'numeric',
+        'double precision' => 'numeric',
+    };
+}
+
+=head2 init_column_to_label
+
+Returns a CODE ref for filtering a column name to its corresponding
+form field label. The CODE ref should expect two arguments:
+the Garden object and the column name.
+
+The default is just to return the column name. If you wanted to return,
+for example, a prettier version aligned with the naming conventions used
+in Rose::DB::Object::ConventionManager, you might do something like:
+
+    my $garden = Rose::DBx::Garden->new(
+                    column_to_label => sub {
+                           my ($garden_obj, $col_name) = @_;
+                           return join(' ', 
+                                       map { ucfirst($_) }
+                                       split(m/_/, $col_name)
+                                  );
+                    }
+                  );
+
+=cut
+
+sub init_column_to_label {
+    sub { return $_[1] }
+}
+
+=head2 init_garden_prefix
+
+The default base class name is C<MyRoseGarden>. This value
+overrides C<class_prefix> and C<base_class> in the base Loader class.
+
+=cut
+
+sub init_garden_prefix {'MyRoseGarden'}
+
+=head2 init_perltidy_opts
+
+If set, Perl::Tidy will be called to format all generated code. The
+value of perltidy_opts should be the same as the command-line options
+to perltidy.
+
+The default is 0 (no run through Perl::Tidy).
+
+=cut
+
+sub init_perltidy_opts {0}
+
+=head2 init_base_code
+
+The return value is inserted into the base RDBO class created.
+
+=cut
+
+sub init_base_code {''}
+
+=head2 plant( I<path> )
+
+I<path> will override module_dir() if set in new().
+
+Returns an array ref of all the class names created.
+
+=head2 make_garden
+
+An alias for plant().
+
+=cut
+
+*make_garden = \&plant;
+
+sub plant {
+    my $self = shift;
+    my $path = shift or croak "path required";
+
+    #carp "path = $path";
+
+    my $path_obj = dir($path);
+
+    $path_obj->mkpath(1);
+
+    # make sure we can 'require' files we generate
+    unshift( @INC, $path );
+
+    # set in loader just in case
+    $self->module_dir($path);
+
+    my $garden_prefix = $self->garden_prefix;
+
+    my $base_code = $self->base_code;
+
+    # make the base class unless it already exists
+    my $base_template = <<EOF;
+package $garden_prefix;
+use strict;
+use base qw( Rose::DB::Object );
+
+$base_code
+
+1;
+EOF
+
+    $self->_make_file( $garden_prefix, $base_template );
+
+    # find all schemas if this db supports them
+    my %schemas;
+    if ( $self->find_schemas ) {
+        my %native = ( information_schema => 1, pg_catalog => 1 );
+        my $info = $self->db->dbh->table_info( undef, '%', undef, 'TABLE' )
+            ->fetchall_arrayref;
+
+        #carp dump $info;
+
+        for my $row (@$info) {
+            next if exists $native{ $row->[1] };
+            $schemas{ $row->[1] }++;
+        }
+    }
+    else {
+        %schemas = ( '' => '' );
+    }
+
+    my ( @rdbo_classes, @form_classes );
+
+    my $preamble = $self->module_preamble;
+
+    my $postamble = $self->module_postamble;
+
+    for my $schema ( keys %schemas ) {
+
+        #carp "working on schema $schema";
+
+        my $schema_class
+            = $schema
+            ? join( '::', $garden_prefix, ucfirst($schema) )
+            : $garden_prefix;
+
+        if ($schema) {
+            my $schema_tmpl
+                = $self->_schema_template( $garden_prefix, $schema_class,
+                $schema );
+
+            $self->_make_file( $schema_class, $schema_tmpl );
+            $self->db_schema($schema);
+        }
+
+        #carp "schema_class: $schema_class";
+
+        $self->class_prefix($schema_class);
+        $self->base_class($schema_class);   # already wrote it, so can require
+
+        my @classes = $self->make_classes;
+
+        for my $class (@classes) {
+
+            #carp "class: $class";
+
+            my $template = my $this_preamble = my $this_postamble = '';
+
+            if ( $class->isa('Rose::DB::Object') ) {
+
+                $template = $class->meta->perl_class_definition( indent => 4 )
+                    . "\n";
+
+                if ($preamble) {
+                    $this_preamble
+                        = ref $preamble eq 'CODE'
+                        ? $preamble->( $class->meta )
+                        : $preamble;
+                }
+
+                if ($postamble) {
+                    my $this_postamble
+                        = ref $postamble eq 'CODE'
+                        ? $postamble->( $class->meta )
+                        : $postamble;
+                }
+
+                push( @rdbo_classes, $class );
+            }
+            elsif ( $class->isa('Rose::DB::Object::Manager') ) {
+                $template
+                    = $class->perl_class_definition( indent => 4 ) . "\n";
+
+                if ($preamble) {
+                    $this_preamble
+                        = ref $preamble eq 'CODE'
+                        ? $preamble->( $class->object_class->meta )
+                        : $preamble;
+                }
+
+                if ($postamble) {
+                    $this_postamble
+                        = ref $postamble eq 'CODE'
+                        ? $postamble->( $class->object_class->meta )
+                        : $postamble;
+                }
+            }
+            else {
+                croak "class $class not supported";
+            }
+
+            $self->_make_file( $class,
+                $this_preamble . $template . $this_postamble );
+        }
+
+    }
+
+    # RDBO classes all done. That was the easy part.
+    # now create a RHTMLO::Form tree using the same model
+    # by default we create a ::Form for each RDBO class,
+    # even though in practice we wouldn't
+    # use some of them (*_map classes e.g.).
+
+    # first create the base ::Form class.
+    my $base_form_class = join( '::', $garden_prefix, 'Form' );
+    my $base_form_template = <<EOF;
+package $base_form_class;
+use strict;
+use base qw( Rose::HTML::Form );
+1;
+EOF
+
+    $self->_make_file( $base_form_class, $base_form_template, 1 );
+
+    for my $rdbo_class (@rdbo_classes) {
+
+        # don't make forms for map tables
+        next if $self->convention_manager->is_map_class($rdbo_class);
+
+        my $form_class = join( '::', $rdbo_class, 'Form' );
+        my $form_template = $self->_form_template( $rdbo_class, $form_class,
+            $base_form_class );
+
+        push( @form_classes, $form_class );
+
+        $self->_make_file( $form_class, $form_template );
+    }
+
+    return [ @rdbo_classes, @form_classes ];
+}
+
+sub _form_template {
+    my ( $self, $rdbo_class, $form_class, $base_form_class ) = @_;
+
+    # load the rdbo class and examine its metadata
+    # make sure rdbo_class is loaded
+    eval "require $rdbo_class";
+    croak "can't load $rdbo_class: $@" if $@;
+
+    # create a form template using the column definitions
+    # as seed for the form field definitions
+    # use the convention manager to assign default field labels
+
+    my $form = <<EOF;
+package $form_class;
+use strict;
+use base qw( $base_form_class );
+
+sub build_form {
+    my \$self = shift;
+    
+    \$self->add_fields(
+    
+EOF
+
+    my @fields;
+    my $count = 0;
+    for my $column ( sort __by_position $rdbo_class->meta->columns ) {
+        push( @fields, $self->_column_to_field( $column, ++$count ) );
+    }
+
+    $form .= join( "\n", @fields );
+    $form .= <<EOF;
+    );
+    
+    return \$self->SUPER::build_form(\@_);
+}
+
+1;
+
+EOF
+
+    return $form;
+}
+
+# keep columns in same order they appear in db
+sub __by_position {
+    my $pos1 = $a->ordinal_position;
+    my $pos2 = $b->ordinal_position;
+
+    if ( defined $pos1 && defined $pos2 ) {
+        return $pos1 <=> $pos2 || lc( $a->name ) cmp lc( $b->name );
+    }
+
+    return lc( $a->name ) cmp lc( $b->name );
+}
+
+sub _column_to_field {
+    my ( $self, $column, $tabindex ) = @_;
+    my $label_maker = $self->column_to_label;
+    my $name        = $column->name;
+    my $col_type    = $column->type;
+    my $type        = $self->column_field_map->{$col_type} || 'text';
+    my $label       = $label_maker->( $self, $name );
+    return <<EOF;
+    $name => {
+        id      => '$name',
+        type    => '$type',   # $col_type
+        label   => '$label',
+        tabindex => $tabindex
+        },
+EOF
+}
+
+sub _schema_template {
+    my ( $self, $base, $package, $schema ) = @_;
+
+    return <<EOF;
+package $package;
+use strict;
+use base qw( $base );
+
+sub schema { '$schema' }
+
+1;
+EOF
+}
+
+sub _make_file {
+    my ( $self, $class, $buffer ) = @_;
+    ( my $file = $class ) =~ s,::,/,g;
+    $file .= '.pm';
+
+    my ( $name, $path, $suffix ) = fileparse( $file, qr{\.pm} );
+
+    my $fullpath = dir( $self->module_dir, $path );
+
+    unless ( $self->force_install ) {
+        return if -s $file;
+    }
+
+    $fullpath->mkpath(1) if $path;
+
+    if ( $self->perltidy_opts ) {
+        require Perl::Tidy;
+        my $newbuf;
+        Perl::Tidy::perltidy(
+            source      => \$buffer,
+            destination => \$newbuf,
+            argv        => $self->perltidy_opts
+        );
+        $buffer = $newbuf;
+    }
+
+    write_file( file( $self->module_dir, $file )->stringify, $buffer )
+        or die "$!\n";
+
+    print "$class written to $file\n";
+}
+
+=head1 AUTHORS
+
+Peter Karman, C<< <karman at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to
+C<bug-rose-dbx-garden at rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Rose-DBx-Garden>.
+I will be notified, and then you'll automatically be notified of progress on
+your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Rose::DBx::Garden
+
+You can also look for information at:
+
+=over 4
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Rose-DBx-Garden>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Rose-DBx-Garden>
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Rose-DBx-Garden>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Rose-DBx-Garden>
+
+=back
+
+=head1 ACKNOWLEDGEMENTS
+
+Thanks to Adam Prime, C<< adam.prime at utoronto.ca >>
+for patches and feedback on the design.
+
+The Minnesota Supercomputing Institute C<< http://www.msi.umn.edu/ >>
+sponsored the development of this software.
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2007 by the Regents of the University of Minnesota.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
+1;    # End of Rose::DBx::Garden
---
+++ t/01-make_garden.t
@@ -0,0 +1,115 @@
+use Test::More tests => 16;
+
+use File::Temp qw( tempdir );
+use Rose::DBx::Garden;
+use Rose::DBx::TestDB;
+use Path::Class;
+use Rose::HTML::Form;
+
+my $debug = $ENV{PERL_DEBUG} || 1;
+
+my $db = Rose::DBx::TestDB->new;
+
+# create a schema that tests out all our column types
+
+ok( $db->dbh->do(
+        qq{
+CREATE TABLE foo (
+    id       integer primary key autoincrement,
+    name     varchar(16),
+    static   char(8),
+    my_int   integer not null default 0,
+    my_dec   float
+    );
+}
+    ),
+    "table foo created"
+);
+
+{
+
+    package MyMetadata;
+    use base qw( Rose::DB::Object::Metadata );
+
+    # we override just this method since we don't actually need/want to
+    # connect to the db, as the standard init_db() would. We need to
+    # re-use our existing $db.
+    sub init_db {
+        my ($self) = shift;
+        $self->{'db_id'} = $db->{'id'};
+        return $db;
+    }
+
+    package MyRDBO;
+    use base qw( Rose::DB::Object );
+
+    sub meta_class {'MyMetadata'}
+}
+
+ok( my $garden = Rose::DBx::Garden->new(
+        db              => $db,
+        find_schemas    => 0,
+        garden_prefix   => 'MyRDBO',
+        force_install   => 1,
+        column_to_label => sub {
+            my ( $garden_obj, $col_name ) = @_;
+            return join(
+                ' ', map { ucfirst($_) }
+                    split( m/_/, $col_name )
+            );
+        }
+    ),
+    "garden obj created"
+);
+
+my $dir = $debug ? '/tmp/rose_garden' : tempdir();
+
+ok( $garden->make_garden($dir), "make_garden" );
+
+# are the files there?
+ok( -s file( $dir, 'MyRDBO.pm' ), "base class exists" );
+ok( -s file( $dir, 'MyRDBO', 'Foo.pm' ), "table class exists" );
+ok( -s file( $dir, 'MyRDBO', 'Foo', 'Form.pm' ),    "form class exists" );
+ok( -s file( $dir, 'MyRDBO', 'Foo', 'Manager.pm' ), "manager class exists" );
+
+# do they compile?
+
+for my $class (
+    qw( MyRDBO MyRDBO::Form MyRDBO::Foo MyRDBO::Foo::Form MyRDBO::Foo::Manager )
+    )
+{
+
+    # have to clean up the symbol table manually
+    # since these classes were created at runtime and are
+    # not in %INC.
+    {
+        no strict 'refs';
+        local *symtable = $class . '::';
+        for my $sym ( keys %symtable ) {
+            delete $symtable{$sym};
+        }
+    }
+
+    eval "use $class";
+    ok( !$@, "require $class" );
+    diag($@) and next if $@;
+
+    if ( $class eq 'MyRDBO::Foo::Form' ) {
+        ok( my $form = $class->new, "new $class" );
+        is( $form->field('my_int')->label, 'My Int', "label callback works" );
+        is( $form->field('my_int')->isa('Rose::HTML::Form::Field::Integer'),
+            1, "my_int -> Integer field" );
+        diag( $form->field('id') );
+
+    SKIP: {
+            if ( $Rose::HTML::Form::VERSION <= '0.550' )
+            {
+                skip( " -- change requested in default column mapping", 1 );
+            }
+
+            is( $form->field('id')->isa('Rose::HTML::Form::Field::Hidden'),
+                1, "id field is hidden" );
+        }
+
+    }
+}
